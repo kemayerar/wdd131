83c87f425fa441b3889ec5049149bc1770630e2f
Kevin <kemayerar@gmail.com>
2024-11-01 16:10:33 -0300

    check

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,1 +1,8 @@
+const d = new Date()
+document.getElementById("currentyear").innerHTML = d.getFullYear();
 
+let oLastModif = new Date(document.lastModified)
+document.getElementById("lastModified").innerHTML =oLastModif
+
+
+    
